332a8c1605152a9eddd94972e05693217ef25c53
baoanj <baoanj@live.com>
2024-08-06 15:58:46 +0800

    fix: user gesture to show a file picker

--- index.js
+++ index.js
@@ -2,19 +2,177 @@ let key = new URLSearchParams(location.search).get('key') || 'key'
 
 document.title = key + ' - ' + document.title
 
-key = 'bytemd-paste-' + key
-
 const pasteWorker = new Worker('worker.js')
 
 pasteWorker.onmessage = e => {
   console.log(e)
-  init(e.data || '')
+  if (e.type === 'error') {
+    alert(e.message)
+  }
 }
 
-pasteWorker.postMessage({
-  type: 'init',
-  key
-})
+var dirHandle
+var db
+
+async function run() {
+  await openIndexDB()
+  await getHandle()
+}
+
+run()
+
+async function readData() {
+  if (dirHandle) {
+    console.log(dirHandle)
+    for await (const entry of dirHandle.values()) {
+      console.log(entry.kind, entry.name)
+    }
+    const fileHandle = await dirHandle.getFileHandle(`copyN-${key}.md`, {
+      create: true
+    })
+    pasteWorker.postMessage({
+      type: 'fileHandle',
+      fileHandle
+    })
+    const file = await fileHandle.getFile()
+    const contents = await file.text()
+    console.log(contents)
+    init(contents || '')
+  }
+}
+
+async function getHandle() {
+  dirHandle = await read('copyN-dir')
+  if (!dirHandle) {
+    document.querySelector('#pick').style.display = 'inline'
+    return
+  }
+  const permission = await verifyPermission(dirHandle)
+  console.log('permission:', permission)
+  if (!permission) dirHandle = null
+  else readData()
+}
+
+// Failed to execute 'showDirectoryPicker' on 'Window': Must be handling a user gesture to show a file picker.
+// 必须手动触发
+async function pickDir() {
+  dirHandle = await window.showDirectoryPicker()
+  await add({ id: 'copyN-dir', handle: dirHandle })
+  document.querySelector('#pick').style.display = 'none'
+  readData()
+}
+
+async function verifyPermission(handle) {
+  const options = { mode: 'readwrite' }
+  // Check if permission was already granted. If so, return true.
+  if ((await handle.queryPermission(options)) === 'granted') {
+    return true
+  }
+  // Request permission. If the user grants permission, return true.
+  if ((await handle.requestPermission(options)) === 'granted') {
+    return true
+  }
+  // The user didn't grant permission, so return false.
+  return false
+}
+
+async function openIndexDB() {
+  return new Promise((resolve, reject) => {
+    var request = window.indexedDB.open('copyn', 1)
+    request.onsuccess = function (event) {
+      db = request.result
+      console.log('数据库打开成功')
+      resolve()
+    }
+    request.onerror = function (event) {
+      console.log('数据库打开报错')
+      reject()
+    }
+    request.onupgradeneeded = function (event) {
+      db = event.target.result
+      var objectStore
+      if (!db.objectStoreNames.contains('handles')) {
+        objectStore = db.createObjectStore('handles', { keyPath: 'id' })
+        // 使用事务的 oncomplete 事件确保在插入数据前对象存储已经创建完毕。
+        objectStore.transaction.oncomplete = event => {
+          console.log('数据库建表成功')
+          resolve()
+        }
+      } else {
+        resolve()
+      }
+    }
+  })
+}
+
+async function add({ id, handle }) {
+  return new Promise((resolve, reject) => {
+    var req = db
+      .transaction(['handles'], 'readwrite')
+      .objectStore('handles')
+      .add({ id, handle })
+
+    req.onsuccess = function (event) {
+      console.log('数据写入成功')
+      resolve()
+    }
+
+    req.onerror = function (event) {
+      console.log('数据写入失败')
+      reject()
+    }
+  })
+}
+
+async function read(id) {
+  return new Promise((resolve, reject) => {
+    var req = db.transaction(['handles']).objectStore('handles').get(id)
+
+    req.onerror = function (event) {
+      console.log('事务失败')
+      reject()
+    }
+
+    req.onsuccess = function (event) {
+      if (req.result) {
+        console.log(req.result)
+        resolve(req.result.handle)
+      } else {
+        console.log('未获得数据记录')
+        resolve()
+      }
+    }
+  })
+}
+
+async function update({ id, handle }) {
+  return new Promise((resolve, reject) => {
+    var req = db
+      .transaction(['handles'], 'readwrite')
+      .objectStore('handles')
+      .put({ id, handle })
+
+    req.onsuccess = function (event) {
+      console.log('数据更新成功')
+      resolve()
+    }
+
+    req.onerror = function (event) {
+      console.log('数据更新失败')
+      reject()
+    }
+  })
+}
+
+// pasteWorker.onmessage = e => {
+//   console.log(e)
+//   init(e.data || '')
+// }
+
+// pasteWorker.postMessage({
+//   type: 'init',
+//   key
+// })
 
 function init(value) {
   const editor = BytemdPaste({
@@ -22,65 +180,68 @@ function init(value) {
     props: {
       // value: localStorage.getItem(key) || '',
       value,
-      plugins: [bytemdPluginGfm({
-        locale: {
-          "strike": "删除线",
-          "strikeText": "文本",
-          "table": "表格",
-          "tableHeading": "标题",
-          "task": "任务列表",
-          "taskText": "待办事项"
-        }
-      }), bytemdPluginHighlight()],
+      plugins: [
+        bytemdPluginGfm({
+          locale: {
+            strike: '删除线',
+            strikeText: '文本',
+            table: '表格',
+            tableHeading: '标题',
+            task: '任务列表',
+            taskText: '待办事项'
+          }
+        }),
+        bytemdPluginHighlight()
+      ],
       locale: {
-        "bold": "粗体",
-        "boldText": "粗体文本",
-        "cheatsheet": "Markdown 语法",
-        "closeHelp": "关闭帮助",
-        "closeToc": "关闭目录",
-        "code": "代码",
-        "codeBlock": "代码块",
-        "codeLang": "编程语言",
-        "codeText": "代码",
-        "exitFullscreen": "退出全屏",
-        "exitPreviewOnly": "恢复默认",
-        "exitWriteOnly": "恢复默认",
-        "fullscreen": "全屏",
-        "h1": "一级标题",
-        "h2": "二级标题",
-        "h3": "三级标题",
-        "h4": "四级标题",
-        "h5": "五级标题",
-        "h6": "六级标题",
-        "headingText": "标题",
-        "help": "帮助",
-        "hr": "分割线",
-        "image": "图片",
-        "imageAlt": "alt",
-        "imageTitle": "图片描述",
-        "italic": "斜体",
-        "italicText": "斜体文本",
-        "limited": "已达最大字符数限制",
-        "lines": "行数",
-        "link": "链接",
-        "linkText": "链接描述",
-        "ol": "有序列表",
-        "olItem": "项目",
-        "preview": "预览",
-        "previewOnly": "仅预览区",
-        "quote": "引用",
-        "quotedText": "引用文本",
-        "shortcuts": "快捷键",
-        "source": "源代码",
-        "sync": "同步滚动",
-        "toc": "目录",
-        "top": "回到顶部",
-        "ul": "无序列表",
-        "ulItem": "项目",
-        "words": "字数",
-        "write": "编辑",
-        "writeOnly": "仅编辑区",
-        "chars": "字符数"
+        bold: '粗体',
+        boldText: '粗体文本',
+        cheatsheet: 'Markdown 语法',
+        closeHelp: '关闭帮助',
+        closeToc: '关闭目录',
+        code: '代码',
+        codeBlock: '代码块',
+        codeLang: '编程语言',
+        codeText: '代码',
+        exitFullscreen: '退出全屏',
+        exitPreviewOnly: '恢复默认',
+        exitWriteOnly: '恢复默认',
+        fullscreen: '全屏',
+        h1: '一级标题',
+        h2: '二级标题',
+        h3: '三级标题',
+        h4: '四级标题',
+        h5: '五级标题',
+        h6: '六级标题',
+        headingText: '标题',
+        help: '帮助',
+        hr: '分割线',
+        image: '图片',
+        imageAlt: 'alt',
+        imageTitle: '图片描述',
+        italic: '斜体',
+        italicText: '斜体文本',
+        limited: '已达最大字符数限制',
+        lines: '行数',
+        link: '链接',
+        linkText: '链接描述',
+        ol: '有序列表',
+        olItem: '项目',
+        preview: '预览',
+        previewOnly: '仅预览区',
+        quote: '引用',
+        quotedText: '引用文本',
+        shortcuts: '快捷键',
+        source: '源代码',
+        sync: '同步滚动',
+        toc: '目录',
+        top: '回到顶部',
+        ul: '无序列表',
+        ulItem: '项目',
+        words: '字数',
+        write: '编辑',
+        writeOnly: '仅编辑区',
+        chars: '字符数'
       }
     }
   })
--- worker.js
+++ worker.js
@@ -1,147 +1,30 @@
+var fileHandle
+
 onmessage = e => {
   console.log(e)
   if (e.data?.type === 'init') {
-    readData(e.data.key)
+    // readData(e.data.key)
+  }
+  if (e.data?.type === 'fileHandle') {
+    fileHandle = e.data.fileHandle
   }
   if (e.data?.type === 'save') {
-    writeData(e.data.key, e.data.value)
+    writeData(e.data.value)
   }
 }
 
-var dirHandle
-var db
-
-async function writeData(key, data) {
-  if (dirHandle) {
-    const fileHandle = await dirHandle.getFileHandle(`copyN-${key}.md`, { create: true });
-    const writable = await fileHandle.createWritable();
+async function writeData(data) {
+  if (fileHandle) {
+    const writable = await fileHandle.createWritable()
     // Write the contents of the file to the stream.
-    await writable.write(data);
+    await writable.write(data)
     // Close the file and write the contents to disk.
-    await writable.close();
+    await writable.close()
+
+    const file = await fileHandle.getFile()
+    const contents = await file.text()
+    if (data !== contents) {
+      postMessage({ type: 'error', message: '保存失败' })
+    }
   }
-}
-
-async function readData(key) {
-  await openIndexDB()
-  await getHandle()
-  if (dirHandle) {
-    const fileHandle = await dirHandle.getFileHandle(`copyN-${key}.md`, { create: true });
-    const file = await fileHandle.getFile();
-    const contents = await file.text();
-    console.log(contents)
-    postMessage(contents)
-  }
-}
-
-async function getHandle() {
-  dirHandle = await read('copyN-dir')
-  if (!dirHandle) {
-    dirHandle = await showDirectoryPicker();
-    await add({ id: 'copyN-dir', handle: dirHandle })
-  }
-  console.log(dirHandle);
-  for await (const entry of dirHandle.values()) {
-    console.log(entry.kind, entry.name);
-  }
-  const permission = await verifyPermission(dirHandle)
-  console.log('permission:', permission)
-  if (!permission) dirHandle = null
-}
-
-async function verifyPermission(handle) {
-  const options = { mode: "readwrite" };
-  // Check if permission was already granted. If so, return true.
-  if ((await handle.queryPermission(options)) === "granted") {
-    return true;
-  }
-  // Request permission. If the user grants permission, return true.
-  if ((await handle.requestPermission(options)) === "granted") {
-    return true;
-  }
-  // The user didn't grant permission, so return false.
-  return false;
-}
-
-async function openIndexDB() {
-  return new Promise((resolve, reject) => {
-    var request = window.indexedDB.open('copyn');
-    request.onsuccess = function (event) {
-      db = request.result;
-      console.log("数据库打开成功");
-      resolve()
-    };
-    request.onerror = function (event) {
-      console.log("数据库打开报错");
-      reject()
-    };
-    request.onupgradeneeded = function (event) {
-      db = event.target.result;
-      var objectStore;
-      if (!db.objectStoreNames.contains("handles")) {
-        objectStore = db.createObjectStore("handles", { keyPath: "id" });
-        console.log("数据库建表成功");
-      }
-      resolve()
-    };
-  })
-}
-
-async function add({ id, handle }) {
-  return new Promise((resolve, reject) => {
-    var req = db
-      .transaction(["handles"], "readwrite")
-      .objectStore("handles")
-      .add({ id, handle });
-
-    req.onsuccess = function (event) {
-      console.log("数据写入成功");
-      resolve()
-    };
-
-    req.onerror = function (event) {
-      console.log("数据写入失败");
-      reject()
-    };
-  })
-}
-
-async function read(id) {
-  return new Promise((resolve, reject) => {
-    var req = db.transaction(["handles"]).objectStore("handles").get(id);
-
-    req.onerror = function (event) {
-      console.log("事务失败");
-      reject()
-    };
-
-    req.onsuccess = function (event) {
-      if (req.result) {
-        console.log(req.result);
-        resolve(req.result.handle)
-      } else {
-        console.log("未获得数据记录");
-        resolve()
-      }
-    };
-  })
-}
-
-async function update({ id, handle }) {
-  return new Promise((resolve, reject) => {
-    var req = db
-      .transaction(["handles"], "readwrite")
-      .objectStore("handles")
-      .put({ id, handle });
-
-    req.onsuccess = function (event) {
-      console.log("数据更新成功");
-      resolve()
-    };
-
-    req.onerror = function (event) {
-      console.log("数据更新失败");
-      reject()
-    };
-  })
 }
